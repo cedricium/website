c89e99e14f4a4434b2cf8ca3594174131fc48e46
Cedricium <amaya.cedric@gmail.com>
2022-10-18 15:10:26 -0700

    Correct `crossorigin` HTML non-boolean attribute

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,11 @@ export default function Document() {
     <Html>
       <Head>
         <link rel='preconnect' href='https://fonts.googleapis.com' />
-        <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin />
+        <link
+          rel='preconnect'
+          href='https://fonts.gstatic.com'
+          crossOrigin=''
+        />
         <link
           href='https://fonts.googleapis.com/css2?family=Inter:wght@400;600&family=Newsreader:ital,opsz@1,6..72&display=swap'
           rel='stylesheet'
