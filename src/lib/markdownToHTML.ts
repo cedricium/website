97193a12049d929ab79ee9511f64d3842c7ecc37
Cedric Amaya <amaya.cedric@gmail.com>
2024-06-14 22:15:05 -0700

    Fix Markdown processing (media, syntax highlighting working)

--- src/lib/markdownToHTML.ts
+++ src/lib/markdownToHTML.ts
@@ -1,7 +1,20 @@
-import { remark } from "remark";
-import html from "remark-html";
+import { unified } from "unified";
+import markdown from "remark-parse";
+import remark2rehype from "remark-rehype";
+import raw from "rehype-raw";
+import stringify from "rehype-stringify";
+import prism from "rehype-prism";
 
-export default async function markdownToHTML(markdown: string) {
-  const result = await remark().use(html).process(markdown);
-  return result.toString();
+import "prismjs/themes/prism-tomorrow.min.css";
+
+export default async function markdownToHTML(content: string) {
+  const processedContent = await unified()
+    .use(markdown)
+    .use(remark2rehype, { allowDangerousHtml: true })
+    .use(raw)
+    .use(prism)
+    .use(stringify)
+    .process(content);
+  const contentHtml = processedContent.toString();
+  return contentHtml;
 }
