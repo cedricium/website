3e56305cdb54b613a77ca9adc75a92832b21ebd9
Cedric Amaya <amaya.cedric@gmail.com>
2024-09-15 22:54:32 -0700

    Finalize changes

--- src/app/status/snapshot-graph.tsx
+++ src/app/status/snapshot-graph.tsx
@@ -8,70 +8,7 @@ import {
   ChartTooltip,
   ChartTooltipContent,
 } from "@/components/ui/chart";
-
-const chartData = [
-  { date: "2024-05-04", avg: 100, fill: "#2BAC76" },
-  { date: "2024-05-05", avg: 100, fill: "#2BAC76" },
-  { date: "2024-05-06", avg: 100, fill: "#2BAC76" },
-  { date: "2024-05-07", avg: 100, fill: "#2BAC76" },
-  { date: "2024-05-08", avg: 100, fill: "#2BAC76" },
-  { date: "2024-05-09", avg: 100, fill: "#2BAC76" },
-  { date: "2024-05-10", avg: 100, fill: "#2BAC76" },
-  { date: "2024-05-11", avg: 100, fill: "#2BAC76" },
-  { date: "2024-05-12", avg: 100, fill: "#2BAC76" },
-  { date: "2024-05-13", avg: 100, fill: "#2BAC76" },
-  { date: "2024-05-14", avg: 100, fill: "#2BAC76" },
-  { date: "2024-05-15", avg: 100, fill: "#2BAC76" },
-  { date: "2024-05-16", avg: 100, fill: "#2BAC76" },
-  { date: "2024-05-17", avg: 100, fill: "#2BAC76" },
-  { date: "2024-05-18", avg: 100, fill: "#2BAC76" },
-  { date: "2024-05-19", avg: 100, fill: "#2BAC76" },
-  { date: "2024-05-20", avg: 100, fill: "#2BAC76" },
-  { date: "2024-05-21", avg: 100, fill: "#2BAC76" },
-  { date: "2024-05-22", avg: 100, fill: "#2BAC76" },
-  { date: "2024-05-23", avg: 100, fill: "#2BAC76" },
-  { date: "2024-05-24", avg: 100, fill: "#2BAC76" },
-  { date: "2024-05-25", avg: 100, fill: "#2BAC76" },
-  { date: "2024-05-26", avg: 100, fill: "#2BAC76" },
-  { date: "2024-05-27", avg: 100, fill: "#2BAC76" },
-  { date: "2024-05-28", avg: 100, fill: "#2BAC76" },
-  { date: "2024-05-29", avg: 100, fill: "#2BAC76" },
-  { date: "2024-05-30", avg: 100, fill: "#2BAC76" },
-  { date: "2024-05-31", avg: 100, fill: "#2BAC76" },
-  { date: "2024-06-01", avg: 100, fill: "#2BAC76" },
-  { date: "2024-06-02", avg: 100, fill: "#2BAC76" },
-  { date: "2024-06-03", avg: 30, fill: "#E95858" },
-  { date: "2024-06-04", avg: 45, fill: "#E95858" },
-  { date: "2024-06-05", avg: 100, fill: "#2BAC76" },
-  { date: "2024-06-06", avg: 100, fill: "#2BAC76" },
-  { date: "2024-06-07", avg: 100, fill: "#2BAC76" },
-  { date: "2024-06-08", avg: 100, fill: "#2BAC76" },
-  { date: "2024-06-09", avg: 100, fill: "#2BAC76" },
-  { date: "2024-06-10", avg: 65, fill: "#FFAF36" },
-  { date: "2024-06-11", avg: 100, fill: "#2BAC76" },
-  { date: "2024-06-12", avg: 100, fill: "#2BAC76" },
-  { date: "2024-06-13", avg: 100, fill: "#2BAC76" },
-  { date: "2024-06-14", avg: 100, fill: "#2BAC76" },
-  { date: "2024-06-15", avg: 100, fill: "#2BAC76" },
-  { date: "2024-06-16", avg: 100, fill: "#2BAC76" },
-  { date: "2024-06-17", avg: 100, fill: "#2BAC76" },
-  { date: "2024-06-18", avg: 100, fill: "#2BAC76" },
-  { date: "2024-06-19", avg: 100, fill: "#2BAC76" },
-  { date: "2024-06-20", avg: 100, fill: "#2BAC76" },
-  { date: "2024-06-21", avg: 75, fill: "#FFAF36" },
-  { date: "2024-06-22", avg: 100, fill: "#2BAC76" },
-  { date: "2024-06-23", avg: 100, fill: "#2BAC76" },
-  { date: "2024-06-24", avg: 100, fill: "#2BAC76" },
-  { date: "2024-06-25", avg: 100, fill: "#2BAC76" },
-  { date: "2024-06-26", avg: 100, fill: "#2BAC76" },
-  { date: "2024-06-27", avg: 100, fill: "#2BAC76" },
-  { date: "2024-06-30", avg: 45, fill: "#E95858" /* red */ },
-  { date: "2024-06-29", avg: 55, fill: "#FFAF36" /* yellow */ },
-  { date: "2024-06-28", avg: 100, fill: "#2BAC76" /* green */ },
-  { date: "2024-06-29", avg: null, fill: "#2BAC76" /* green */ },
-  { date: "2024-06-30", avg: null, fill: "#2BAC76" /* green */ },
-  { date: "2024-07-01", avg: undefined, fill: "#2BAC76" /* green */ },
-];
+import { SnapshotChartReq } from "./types";
 
 const chartConfig = {
   status: {
@@ -79,11 +16,11 @@ const chartConfig = {
   },
 } satisfies ChartConfig;
 
-export default function SnapshotGraph({ data }) {
+export default function SnapshotGraph({ data }: SnapshotChartReq) {
   return (
     <ChartContainer
       config={chartConfig}
-      className="min-h-[72px] h-[72px] w-full"
+      className="min-h-[80px] h-[80px] w-full"
     >
       <BarChart accessibilityLayer data={data}>
         <XAxis
@@ -91,7 +28,7 @@ export default function SnapshotGraph({ data }) {
           tickLine={false}
           axisLine={false}
           tickMargin={8}
-          minTickGap={72}
+          minTickGap={60}
           tickFormatter={(value) => {
             const date = new Date(value);
             return date.toLocaleDateString("en-US", {
---
+++ src/app/status/types.ts
@@ -0,0 +1,32 @@
+export type MonitorsReq = {
+  monitors: Monitor[];
+  ranges: StatusRange[];
+};
+
+export type SnapshotChartData = { date: string; avg?: number; fill?: string };
+
+export type SnapshotChartReq = {
+  data: SnapshotChartData[];
+};
+
+export type StatusRange = {
+  min: number;
+  max: number;
+  label: string;
+  color: string;
+};
+
+export type Monitor = {
+  id: string;
+  title: string;
+  description?: string;
+  created_at: number;
+  last_update_at: number;
+  period: number;
+  frequency: number;
+  status: {
+    value: number;
+    label: string;
+    color: string;
+  };
+};
