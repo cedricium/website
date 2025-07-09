7fadb8438d77117fa11e6833ffb1e124ff038c23
Cedric Amaya <amaya.cedric@gmail.com>
2025-07-09 12:18:59 -0700

    feat: add redirect link to published resume

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/resume",
+        destination:
+          "https://docs.google.com/document/d/e/2PACX-1vQ_306Klob3oFsxgvdO1OP2Xp2eumCjbtMQOyyH5pO8ZSqpHDFsMTgvia4O-kD-K-a9dbIfzpDNvEbj/pub",
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
