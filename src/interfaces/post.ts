10aede44c5a4135ae85d64128c669e49f8b5b0e7
Cedric Amaya <amaya.cedric@gmail.com>
2025-11-14 23:29:55 -0800

    chore: archive legacy portfolio site

--- src/interfaces/post.ts
+++
@@ -1,8 +0,0 @@
-export type Post = {
-  slug: string;
-  title: string;
-  date: string;
-  excerpt?: string;
-  content: string;
-  draft?: boolean;
-};
--- src/interfaces/project.ts
+++
@@ -1,5 +0,0 @@
-export type Project = {
-  name: string;
-  url: string;
-  description: string;
-};
